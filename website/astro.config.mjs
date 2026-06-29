13c13b2dad8ba08bd2ad2cd0ac4b97d88c98925f
Niki <63308844+nikilia@users.noreply.github.com>
2026-06-29 19:33:32 +0200

    Update astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://ndastudio.github.io',
-  base: '/studio-website/',
-  build: {
-    format: 'directory',
-  },
-});
+  site: 'https://NDAstudio.github.io',
+  base: '/studio-website',
+})
